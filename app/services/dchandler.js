7dfa13a36f8bf6441eb6651da7793dc2baab7b52
Elia <reutlinger.elia@gmail.com>
2021-07-12 18:46:33 +0200

    Update dchandler.js

--- app/services/dchandler.js
+++ app/services/dchandler.js
@@ -1,39 +1,42 @@
 const sn = '[DISCORD-HANDLER] -> '
 
 async function consoleMsg(msg) {
-    if (
-        msg.member.hasPermission('ADMINISTRATOR') &&
-        msg.author.id !== process.env.DISCORD_BOT_ID
-    ) {
-        global.log.debug(sn + 'Console message detected!')
-        global.commands[msg.id] = {
-            message: 'console_msg',
-            user: msg.author.username,
-            content: msg.content
-        }
-        await msg.delete()
+  if (
+    msg.member.hasPermission('ADMINISTRATOR') &&
+    msg.author.id !== process.env.DISCORD_BOT_ID
+  ) {
+    global.log.debug(sn + 'Console message detected!')
+    global.commands[msg.id] = {
+      message: 'console_msg',
+      user: msg.author.username,
+      content: msg.content
     }
+    await msg.delete()
+  }
 }
 
 async function chatMsg(msg) {
-    if (msg.author.id !== process.env.DISCORD_BOT_ID) {
-        if (msg.content.toLowerCase().includes('#')) {
-            await msg.delete()
-            return
-        }
-        global.log.debug(sn + 'Chat message detected!')
-        global.commands[msg.id] = {
-            message: 'console_msg',
-            user: '[DC] ' + msg.author.username,
-            content: msg.content
-        }
-        await msg.delete()
+  if (msg.author.id !== process.env.DISCORD_BOT_ID) {
+    if (
+      msg.content.trim().startsWith('#') ||
+      msg.content.trim().startsWith('/')
+    ) {
+      await msg.delete()
+      return
     }
+    global.log.debug(sn + 'Chat message detected!')
+    global.commands[msg.id] = {
+      message: 'console_msg',
+      user: '[Discord] ' + msg.author.username,
+      content: msg.content
+    }
+    await msg.delete()
+  }
 }
 
 exports.start = async function start(dcClient) {
-    dcClient.on('message', async msg => {
-        if (msg.channel.id == process.env.DISCORD_CH_CONSOLE) consoleMsg(msg)
-        else if (msg.channel.id == process.env.DISCORD_CH_CHAT) chatMsg(msg)
-    })
+  dcClient.on('message', async msg => {
+    if (msg.channel.id == process.env.DISCORD_CH_CONSOLE) consoleMsg(msg)
+    else if (msg.channel.id == process.env.DISCORD_CH_CHAT) chatMsg(msg)
+  })
 }

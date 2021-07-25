4a92ac4b128e3f56a2f58e489e2c9c4d4696b631
Elia <reutlinger.elia@gmail.com>
2021-07-25 14:33:53 +0200

    adsafds

--- app/bot.js
+++ app/bot.js
@@ -2,64 +2,20 @@ const sn = '[BOT] -> '
 
 const Discord = require('discord.js')
 const dcClient = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
-const ftpWatcher = require('./services/ftpwatcher')
-const logProcessor = require('./services/logprocessor/logprocessor')
-const dcWriter = require('./services/dcwriter/dcwriter')
-const state = require('./services/state')
-const statistics = require('./services/statistics/statistics')
-const cmdHandler = require('./services/cmdhandler/cmdhandler')
-const dcHandler = require('./services/dchandler')
 const dcBot = require('./services/dcbot')
 
 exports.start = async function start() {
-    /*
-        Possible args:
-        - dcBot     -> Discord functions for handling new Users etc.
-        - logBot    -> Functions for processing log-data to discord and commands
-        - inGame    -> Scum-Bot handling commands and more (only with -logBot enabled)
-    */
+  dcClient.on('ready', () => {
+    global.log.debug(sn + `Logged in as ${dcClient.user.tag}!`)
 
-    let args = process.argv.slice(2).map(el => el.replace('-', '').trim())
-    dcClient.on('ready', () => {
-        global.log.debug(sn + `Logged in as ${dcClient.user.tag}!`)
+    startDCBot(dcClient)
+  })
 
-        if (args.includes('dcBot')) startDCBot(dcClient)
-
-        if (args.includes('logBot')) startLogFunctions(dcClient)
-
-        if (args.includes('inGame')) startIngameBot(dcClient)
-    })
-
-    global.log.debug(sn + 'Login on Discord')
-    dcClient.login(process.env.DISCORD_TOKEN)
+  global.log.debug(sn + 'Login on Discord')
+  dcClient.login(process.env.DISCORD_TOKEN)
 }
 
 async function startDCBot(dcClient) {
-    global.log.debug(sn + 'Starting Discord-Bot')
-    dcBot.start(dcClient)
-}
-
-async function startLogFunctions(dcClient) {
-    global.log.debug(sn + 'Starting Discord-Handler functionalities')
-    dcHandler.start(dcClient)
-
-    global.log.debug(sn + 'Starting Discord-Writer')
-    dcWriter.start(dcClient)
-
-    global.log.debug(sn + 'Starting State-Display')
-    state.start(dcClient)
-
-    global.log.debug(sn + 'Starting Statistics')
-    statistics.start(dcClient)
-
-    global.log.debug(sn + 'Starting FTP-Watcher')
-    ftpWatcher.start()
-
-    global.log.debug(sn + 'Starting Log-Processor')
-    logProcessor.start()
-}
-
-async function startIngameBot(dcClient) {
-    global.log.debug(sn + 'Starting Command-handler')
-    cmdHandler.start()
+  global.log.debug(sn + 'Starting Discord-Bot')
+  dcBot.start(dcClient)
 }
--- main.js
+++ main.js
@@ -28,31 +28,8 @@ global.log.debug(sn + 'Bot initialized, starting processes')
 global.log.debug(sn + '----------------------------------------------------------\n')
 
 require('dotenv').config()
-global.admins = require('./app/plugins/admins')
 global.sleep = require('./app/plugins/sleep')
 global.nZero = require('./app/plugins/nzero')
-global.io = require('@pm2/io')
-global.doSend = true
-global.updateCache = true
-global.gameReady = false
-global.ingameTime = false
-global.playersOnline = 0
-global.newCmds = false
-global.playerlist = {}
-global.updatingFTP = false
-global.updates = true
-global.commands = {}
-global.newEntries = {
-  mines: {},
-  chat: {},
-  admin: {},
-  login: {},
-  kill: {},
-  maps: {}
-}
-
-botMessages = require('./botMessages')
-global.bot = botMessages.bot
 
 const bot = require('./app/bot')
 bot.start()
